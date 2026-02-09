6027e4fdb5eb99a8635b5e137311146168847182
Krishna-Mani12 <krishnamani121104@gmail.com>
2026-02-09 14:40:32 +0530

    commit1

---
+++ Assets/app.js
@@ -0,0 +1,8 @@
+// Select elements
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+// Add click event
+button.addEventListener("click", function () {
+    output.textContent = "Button clicked! JavaScript is working 🚀";
+});
